2849f95191dc17f728f9ce873f9a3f8318cec445
flora <ogniflora@hotmail.it>
2014-08-08 11:54:52 +0100

    Four plots and the relative four R-codes for the 1st week assignment of the course 'Exploratory Data Analysis'

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
-## This assignment uses data from the UC Irvine Machine Learning Repo,
-## a popular repository for machine learning datasets. I'll use the 
+## I use the data from the UC Irvine Machine Learning Repo,
+## a repository for machine learning datasets. I'll use the 
 ## “Individual household electric power consumption Data Set” 
 ## from the dates 2007-02-01 and 2007-02-02 (that is between the rows:
 ## 66638 and (66638+2880))
@@ -11,20 +11,21 @@ my_table <- read.table("household_power_consumption.txt", sep = ";",
                        col.names = c("Date", "Time", "Global_Active_Power", 
                                      "Global_Reactive_Power", "Voltage", 
                                      "Global intensity", "Sub_metering_1", 
+ 
                                      "Sub_metering_2", "Sub_metering_3"))
-# date <- as.Date(my_table$Date)
-# time <- strptime(my_table$Time, format = "%H:%M:%S")
 
+## x axis: 
+## I create a vector, time, with the date and time of day stored as time objects
+
+time <- strptime(paste(my_table$Date, my_table$Time), 
+                 format = "%d/%m/%Y %H:%M:%S")
 
 ## plot on a png file device
 png(filename = "plot2.png", width = 480, height = 480)
 
 library(datasets)
-x <- c(1:2880)
 
-plot(x, my_table$Global_Active_Power, type ="l", xlab = "",
-     ylab ="Global Active Power (kilowatts)", xaxt = "n")
-x_ticks <- c(1, 2880, by = "1440")
-axis(1, at=x_ticks, labels = c("Thu",  "Sat", "Fri"))
+plot(time, my_table$Global_Active_Power, type ="l", xlab = "",
+     ylab ="Global Active Power (kilowatts)")
 
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
-## This assignment uses data from the UC Irvine Machine Learning Repo,
-## a popular repository for machine learning datasets. I'll use the 
+## I use the data from the UC Irvine Machine Learning Repo,
+## a repository for machine learning datasets. I'll use the 
 ## “Individual household electric power consumption Data Set” 
 ## from the dates 2007-02-01 and 2007-02-02 (that is between the rows:
 ## 66638 and (66638+2880))
@@ -11,7 +11,7 @@ my_table <- read.table("household_power_consumption.txt", sep = ";",
                                      "Global intensity", "Sub_metering_1", 
                                      "Sub_metering_2", "Sub_metering_3"))
 
-## Four plots in a canvas - x = Time of the day
+## Four plots in a canvas - x = time = Time of the day
 ## Plot I:  y = Global Active Power (Same as plot2)
 ## Plot II: y = Energy sub metering for three data sets (Sub metering 1, 2 and 3) (Same as plot 3)
 ## Plot III: y = Voltage
@@ -24,54 +24,42 @@ library(datasets)
 
 par(mfcol = c(2, 2))
 
-x <- c(1:2880)
+## x axis: 
+## I create a vector, time, with the date and time of day stored as time objects
+
+time <- strptime(paste(my_table$Date, my_table$Time), 
+                 format = "%d/%m/%Y %H:%M:%S")
 
 ## Plot I
 
-plot(x, my_table$Global_Active_Power, type ="l", xlab = "",
-     ylab ="Global Active Power", xaxt = "n")
-
-x_ticks <- c(1, 2880, by = "1440")
-
-axis(1, at=x_ticks, labels = c("Thu",  "Sat", "Fri"))
+plot(time, my_table$Global_Active_Power, type ="l", xlab = "",
+     ylab ="Global Active Power")
 
 
 ## Plot II
 
-plot(x, my_table$Sub_metering_1, 
+plot(time, my_table$Sub_metering_1, 
      ylim = range(c(my_table$Sub_metering_1, my_table$Sub_metering_2, 
                     my_table$Sub_metering_3)), type ="l", xlab = "",
-     ylab ="Energy sub metering", xaxt = "n")
-lines(x, my_table$Sub_metering_2, col = "red")
-lines(x, my_table$Sub_metering_3, col = "blue")
+     ylab ="Energy sub metering")
+lines(time, my_table$Sub_metering_2, col = "red")
+lines(time, my_table$Sub_metering_3, col = "blue")
 
 legend("topright", lty = 1, legend=c("Sub_metering_1", "Sub_metering_2", 
         "Sub_metering_3"), col=c("black", "red", "blue"),
         bty = 'n')
 
-x_ticks <- c(1, 2880, by = "1440")
-
-axis(1, at=x_ticks, labels = c("Thu",  "Sat", "Fri"))
-
 
 ## Plot III
 
-plot(x, my_table$Voltage, type ="l", xlab = "datetime",
-     ylab ="Voltage", xaxt = "n")
-
-x_ticks <- c(1, 2880, by = "1440")
-
-axis(1, at=x_ticks, labels = c("Thu",  "Sat", "Fri"))
+plot(time, my_table$Voltage, type ="l", xlab = "datetime",
+     ylab ="Voltage")
 
 
 ## Plot IV
 
-plot(x, my_table$Global_Reactive_Power, type ="l", xlab = "datetime",
-     ylab ="Global_Reactive_Power", xaxt = "n")
-
-x_ticks <- c(1, 2880, by = "1440")
-
-axis(1, at=x_ticks, labels = c("Thu",  "Sat", "Fri"))
+plot(time, my_table$Global_Reactive_Power, type ="l", xlab = "datetime",
+     ylab ="Global_Reactive_Power")
 
 
 dev.off()
